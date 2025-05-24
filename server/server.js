a3daac930251d2fa9ee25138c92b7af40b2285ee
kim <Kimvz19@users.noreply.github.com>
2025-05-24 14:00:28 +0200

    updating the server rendering

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ import { Liquid } from 'liquidjs';
 import sirv from 'sirv';
 
 // Imports for data opslaan & vewerken
-import bodyParser from 'body-parser';
-import { LocalStorage } from 'node-localstorage';
+// import bodyParser from 'body-parser';
+// import { LocalStorage } from 'node-localstorage';
 
 // ⭐️ LIQUID SETUP ⭐️ //
 const engine = new Liquid({ extname: '.liquid' });
@@ -17,32 +17,25 @@ const app = new App();
 
 //mapje wordt aangemaakt in de root van het project
 // hierin wordt de favoriten data opgeslagen
-const localStorage = new LocalStorage('./scratch');
+// const localStorage = new LocalStorage('./scratch');
 
 //  verwerkt post requests
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // ⭐️ variables ⭐️//
-// uit .env bestand
-const apiKey = process.env.API_KEY;
-const apiSecret = process.env.API_SECRET;
-const baseUrl = process.env.BASE_URL;
-const geoApiKey = process.env.GEODB_API_KEY;
+
 
 
 // ⭐️ LIQUID TEMPLATE RENDERING ⭐️ //
 const renderTemplate = (template, data) => {
-  // favorites ophalen uit localStorage
-  const favoritesFromStorage = JSON.parse(localStorage.getItem('favorites') || '[]')
-    .map(id => String(id));
+
 
   // renderdata samenstellen
   return engine.renderFileSync(template, {
     NODE_ENV: process.env.NODE_ENV || 'production',
-    ...data,
-    favorites: favoritesFromStorage
+    ...data
   });
 };
 
@@ -52,45 +45,11 @@ const renderTemplate = (template, data) => {
 app.get('/', async (req, res) => {
   try {
     // Pagina ophalen uit de query, standaard is 1
-    const page = req.query.page || 1;
-
-    // 🐶 OAuth2-authenticatie petfinder api 🐶
-    const tokenResponse = await fetch('https://api.petfinder.com/v2/oauth2/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        grant_type: 'client_credentials',
-        client_id: apiKey,
-        client_secret: apiSecret
-      })
-    });
-
-    const tokenData = await tokenResponse.json();
-    const accessToken = tokenData.access_token;
-
-    // Dieren ophalen, 35 per pagina
-    const petfinderResponse = await fetch(`${baseUrl}animals?page=${page}&limit=38`, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    });
-
-    const petfinderData = await petfinderResponse.json();
-
-    // Filter dieren met geen foto
-    const filteredAnimals = petfinderData.animals.filter(animal => {
-      return animal.primary_photo_cropped || (animal.photos && animal.photos.length > 0);
-    });
-
-    // nieuwe data met gefilterde dieren
-    const filteredPetfinderData = {
-      ...petfinderData,
-      animals: filteredAnimals
-    };
+    // const page = req.query.page || 1;
 
     // rederen naar index.liquid
     res.send(renderTemplate('server/views/index.liquid', {
-      title: 'Newhome',
-      petfinderData: filteredPetfinderData,
-      currentPage: Number(page)
+      title: 'home'
     }));
 
     //fout melding 
@@ -104,74 +63,9 @@ app.get('/', async (req, res) => {
 // 🐶 DETAIL PAGE 🐶//
 app.get('/detail/:id', async (req, res) => {
   try {
-    // 🐶 OAuth2-authenticatie petfinder api 🐶
-    const tokenResponse = await fetch('https://api.petfinder.com/v2/oauth2/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        grant_type: 'client_credentials',
-        client_id: apiKey,
-        client_secret: apiSecret
-      })
-    });
-
-    const tokenData = await tokenResponse.json();
-    const accessToken = tokenData.access_token;
-
-    const response = await fetch(`${baseUrl}animals/${req.params.id}`, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    });
-
-    //  ⭐️ variabelen  ⭐️ //
-    const data = await response.json();
-    const animalID = data.animal;
-
-    let cityInfo = null;
-    let regionInfo = null;
-
-    // Gegevens ophalen van de GeoDB API
-    const city = animalID.contact.address.city;
-    const country = animalID.contact.address.country;
-
-    if (city && country) {
-      try {
-        const geoResponse = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${city}&countryIds=${country}&limit=1`, {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': geoApiKey,
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-          }
-        });
-
-        // als gegevens beschikbaar zijn, deze ophalen
-        const geoData = await geoResponse.json();
-        cityInfo = geoData.data[0] || null;
-
-        if (cityInfo?.regionCode) {
-          const regionResponse = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions/${cityInfo.regionCode}`, {
-            method: 'GET',
-            headers: {
-              'X-RapidAPI-Key': geoApiKey,
-              'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-            }
-          });
-
-          const regionData = await regionResponse.json();
-          regionInfo = regionData.data;
-        }
-
-        //fout melding 
-      } catch (err) {
-        console.error('GeoDB API error:', err);
-      }
-    }
-
      // data + geo gegevens renderen
     res.send(renderTemplate('server/views/detail.liquid', {
-      title: animalID.name || 'Dier detail',
-      animalID,
-      cityInfo,
-      regionInfo
+      title: 'Detail',
     }));
 
     //fout melding 
@@ -183,67 +77,32 @@ app.get('/detail/:id', async (req, res) => {
 
 
 // ⭐ Favorite POST ⭐ //
-app.post('/favorite', (req, res) => {
-  // controleer of er een animalId is meegegeven,
-  // anders error melding
-  const animalId = req.body.animalId;
-  if (!animalId) return res.status(400).send('Geen dier ID');
+// app.post('/favorite', (req, res) => {
+//   const animalId = req.body.animalId;
+//   if (!animalId) return res.status(400).send('Geen dier ID');
 
-  // key met json array van dier id's
-  // default state is leeg
-  let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+//   let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
 
-  // toevoegen of verwijderen
-  if (favorites.includes(animalId)) {
-    favorites = favorites.filter(id => id !== animalId);
 
-  } else {
-    favorites.push(animalId);
-  }
+//   if (favorites.includes(animalId)) {
+//     favorites = favorites.filter(id => id !== animalId);
 
-  localStorage.setItem('favorites', JSON.stringify(favorites));
-  res.redirect(req.headers.referer || '/');
-});
+//   } else {
+//     favorites.push(animalId);
+//   }
+
+//   localStorage.setItem('favorites', JSON.stringify(favorites));
+//   res.redirect(req.headers.referer || '/');
+// });
 
 
 
 
 // ⭐ FAVORIETEN OVERZICHT
 app.get('/favorites', async (req, res) => {
-  const favoritesFromStorage = JSON.parse(localStorage.getItem('favorites') || '[]');
-
-  if (favoritesFromStorage.length === 0) {
-    return res.send(renderTemplate('server/views/favorites.liquid', {
-      title: 'Favorieten',
-      animals: []
-    }));
-  }
-
-  const tokenResponse = await fetch('https://api.petfinder.com/v2/oauth2/token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams({
-      grant_type: 'client_credentials',
-      client_id: apiKey,
-      client_secret: apiSecret
-    })
-  });
-
-  const tokenData = await tokenResponse.json();
-  const accessToken = tokenData.access_token;
-
-  const requests = favoritesFromStorage.map(id =>
-    fetch(`${baseUrl}animals/${id}`, {
-      headers: { Authorization: `Bearer ${accessToken}` }
-    }).then(res => res.json())
-  );
-
-  const results = await Promise.all(requests);
-  const animals = results.map(r => r.animal).filter(Boolean);
 
   res.send(renderTemplate('server/views/favorites.liquid', {
-    title: 'Mijn Favorieten',
-    animals
+    title: 'favs',
   }));
 });
 
