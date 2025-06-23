e00b4ef1a47981efe7f5b4e8df5fcf6cabe9dfa5
kim <Kimvz19@users.noreply.github.com>
2025-06-23 22:59:50 +0200

    fixing the experience & adding winning project

--- client/index.js
+++ client/index.js
@@ -3,3 +3,18 @@ import './index.css';
 
 // controle of js werkt
 console.log('index.js is working');
+
+
+
+document.querySelectorAll('.experiences-wrapper li').forEach((item) => {
+    const details = item.querySelector('details');
+
+    item.addEventListener('mouseenter', () => {
+      details.setAttribute('open', true);
+    });
+
+    item.addEventListener('mouseleave', () => {
+      details.removeAttribute('open');
+    });
+  });
+
